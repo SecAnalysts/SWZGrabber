4c2d6d850460fc65f021b35d8d37236bb5cdac60
budybacod <30666229+budybacod@users.noreply.github.com>
2019-03-12 18:37:15 +0700

    Create swzgrab.py

---
+++ swzgrab.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#Coded BY budybacod
+#secanalyst 2014
+
+from urllib2 import Request, urlopen, URLError, HTTPError
+
+def Space(j):
+	i = 0
+	while i<=j:
+		print " ",
+		i+=1
+
+
+def grabfile():
+	f = open("grab.txt","r");
+	link = raw_input("Enter Site Name \n(ex : example.com or www.example.com ): ")
+	print "\n\nAvilable links : \n"
+	while True:
+		sub_link = f.readline()
+		if not sub_link:
+			break
+		req_link = "http://"+link+"/"+sub_link
+		req = Request(req_link)
+		try:
+			response = urlopen(req)
+		except HTTPError as e:
+			continue
+		except URLError as e:
+			continue
+		else:
+			print "OK => ",req_link
+
+def Credit():
+	Space(9); print "##########################################"
+	Space(9); print "#        +++ SWZ Grabber +++             #"
+	Space(9); print "#        Script Coded By @pelandok       #"
+	Space(9); print "#    Secanalyst Copyright 2014           #"
+	Space(9); print "##########################################"
+
+Credit()
+grabfile()
